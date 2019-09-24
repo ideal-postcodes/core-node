fd872ad57685e239f63ee905d881325805d87b01
Christopher Blanchard <cablanchard@gmail.com>
2019-09-24 18:30:04 +0200

    docs(Agent): Clean up docs
    - Hide some aspects in typedoc output

--- lib/agent.ts
+++ lib/agent.ts
@@ -5,21 +5,35 @@ import {
   HttpResponse,
   errors,
 } from "@ideal-postcodes/core-interface";
-const { IdealPostcodesError } = errors;
 
+const {
+  /**
+   * Ideal Postcodes base error class implemented in [core-interface](https://core-interface.ideal-postcodes.dev/classes/idealpostcodeserror.html)
+   */
+  IdealPostcodesError,
+} = errors;
+
+// @hidden
 interface GotHeaders {
   [key: string]: string | string[] | undefined;
 }
 
 /**
- * GotConfig
- *
  * An optional configuration object which is passed to the underlying got http
  * client
+ *
+ * `got` is the underlying HTTP client that powers core-node. Be careful when
+ * configuring `gotConfig` so as not to manually override critical request
+ * attributes like method, query, header, etc. `got` confirmation options
+ * available here: [github.com/sindresorhus/got#options](https://github.com/sindresorhus/got#options)
  */
 export type GotConfig = Partial<GotJSONOptions>;
 
-// Converts a Got header object to one that can be used by the client
+/**
+ * Converts a Got header object to one that can be used by the client
+ *
+ * @hidden
+ */
 export const toHeader = (gotHeaders: GotHeaders): Record<string, string> => {
   return Object.keys(gotHeaders).reduce<Record<string, string>>(
     (headers, key) => {
@@ -35,6 +49,11 @@ export const toHeader = (gotHeaders: GotHeaders): Record<string, string> => {
   );
 };
 
+/**
+ * Adapts got responses to a format consumable by core-interface
+ *
+ * @hidden
+ */
 const toHttpResponse = (
   httpRequest: HttpRequest,
   response: Response<object>
@@ -48,6 +67,12 @@ const toHttpResponse = (
   };
 };
 
+/**
+ * Catch non-response errors (e.g. network failure, DNS failure, timeout)
+ * wrap in our Error class and return
+ *
+ * @hidden
+ */
 const handleError = (error: Error): Promise<never> => {
   const idpcError = new IdealPostcodesError({
     message: `[${error.name}] ${error.message}`,
@@ -57,6 +82,13 @@ const handleError = (error: Error): Promise<never> => {
   return Promise.reject(idpcError);
 };
 
+/**
+ * Agent
+ *
+ * Implements node.js HTTP agent for core-interface client
+ *
+ * @hidden
+ */
 export class Agent implements IAgent {
   public got: GotInstance;
   public gotConfig: GotConfig;
--- lib/client.ts
+++ lib/client.ts
@@ -8,7 +8,10 @@ import {
 } from "@ideal-postcodes/core-interface";
 import { Agent, GotConfig } from "./agent";
 
-const userAgent = `IdealPostcodes ideal-postcodes/core-node`;
+/**
+ * Default user agent string
+ */
+const USER_AGENT = `IdealPostcodes ideal-postcodes/core-node`;
 
 export interface Config {
   /**
@@ -46,14 +49,14 @@ export class Client extends CoreInterface {
    * Client constructor extends CoreInterface by also accepting an optional got
    * configuration object as the second argument.
    *
-   * got is the underlying HTTP client that powers core-node. Be careful when
-   * configuring gotConfig so as not to manually override critical request
-   * attributes like method, query, header, etc. got confirmation options
+   * `got` is the underlying HTTP client that powers core-node. Be careful when
+   * configuring `gotConfig` so as not to manually override critical request
+   * attributes like method, query, header, etc. `got` confirmation options
    * available here: [github.com/sindresorhus/got#options](https://github.com/sindresorhus/got#options)
    */
   constructor(config: Config, gotConfig: GotConfig = {}) {
     const agent = new Agent(gotConfig);
-    const header = { "User-Agent": userAgent };
+    const header = { "User-Agent": USER_AGENT };
     const tls = config.tls === undefined ? TLS : config.tls;
     const baseUrl = config.baseUrl === undefined ? API_URL : config.baseUrl;
     const version = config.version === undefined ? VERSION : config.version;
