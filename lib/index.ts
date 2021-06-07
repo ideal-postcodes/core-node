f934f95dc43518000c0cb811ffab15cc00515b64
mfilip <marcin.filip@gmail.com>
2021-06-07 11:32:12 +0200

    update

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,15 @@
-import { Client } from "./client";
-import { Agent } from "./agent";
+/**
+ * @module Library Exports
+ */
 
-export { Client, Agent };
+/**
+ * Export core-interface helpers and resources
+ *
+ */
+export * from "@ideal-postcodes/core-interface";
+
+/**
+ * Export HTTP Client with HTTP agent that supports `fetch`
+ */
+export { Client } from "./client";
+export { Agent } from "./agent";
